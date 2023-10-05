c082b63e7f1c222344e267907c408a1543e1792e
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-10-05 12:16:34 +0300

    Added Seminar 4

--- src/seminar4/Task6.java
+++ src/seminar4/Task6.java
@@ -63,6 +63,69 @@ class Calculator {
     }
 }
 
+//import java.util.ArrayDeque;
+//        import java.util.Deque;
+//
+//class Calculator {
+//    public Deque<Integer> resultsStack = new ArrayDeque<>();
+//    public Deque<Character> operationsStack = new ArrayDeque<>();
+//    public int prevResult;
+//
+//    public int calculate(char op, int a, int b) {
+//        if (op == '<') {
+//            if (resultsStack.size() >= 2) {
+//                resultsStack.pop();
+//                prevResult = resultsStack.peek();
+//            }
+//            return prevResult;
+//        } else {
+//            int result = performOperation(op, a, b);
+//            resultsStack.push(result);
+//            operationsStack.push(op);
+//            prevResult = result;
+//            return result;
+//        }
+//    }
+//
+//
+//    private int performOperation(char op, int a, int b) {
+//        switch (op) {
+//            case '+':
+//                return add(a, b);
+//            case '-':
+//                return minus(a, b);
+//            case '*':
+//                return mult(a, b);
+//            case '/':
+//                return divide(a, b);
+//            default:
+//                throw new IllegalArgumentException("Некорректный оператор: " + op);
+//        }
+//    }
+//
+//    private int divide(int a, int b) {
+//        if (b != 0) {
+//            return a / b;
+//        } else {
+//            throw new ArithmeticException("Деление на ноль");
+//        }
+//    }
+//
+//    private int mult(int a, int b) {
+//        return a * b;
+//    }
+//
+//    private int minus(int a, int b) {
+//        return a - b;
+//    }
+//
+//    private int add(int a, int b) {
+//        return a + b;
+//    }
+//}
+//
+
+
 // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
 public class Task6 {
     public static void main(String[] args) {

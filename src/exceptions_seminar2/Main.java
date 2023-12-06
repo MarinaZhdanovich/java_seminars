eed5d8d936a6894e584ccc7b4b25947887b4113a
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-12-06 21:11:05 +0300

    Added Exceptions seminar2

---
+++ src/exceptions_seminar2/Main.java
@@ -0,0 +1,65 @@
+package exceptions_seminar2;
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Запишите в файл следующие строки:
+Анна=4
+Елена=5
+Марина=6
+Владимир=?
+Константин=?
+Иван=4
+Реализуйте метод, который считывает данные из файла и сохраняет в двумерный массив (либо HashMap, если
+студенты с ним знакомы). В отдельном методе нужно будет пройти по структуре данных, если сохранено
+значение ?, заменить его на соответствующее число.Если на каком-то месте встречается символ, отличный от
+числа или ?, бросить подходящее исключение.Записать в тот же файл данные с замененными символами ?.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String[]> readFile (String patch) {
+        List<String[]> listNames = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(patch));
+            String line = " ";
+            while ((line = reader.readLine()) != null) {
+                listNames.add(line.split("="));
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return listNames;
+    }
+
+    public static void modificationArray(List<String[]> arrayNames) {
+        for (String[] itemArray : arrayNames) {
+            if (!itemArray[1].equals("?") && !checkNumber(itemArray[1]))
+                throw new IllegalArgumentException("Элемент не ? и не число");
+            itemArray[1] = String.valueOf(itemArray[0].length());
+        }
+    }
+
+    public static boolean checkNumber(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void writeFile(List<String[]>list, String patch) {
+        try {
+            
+        }
+    }
+}

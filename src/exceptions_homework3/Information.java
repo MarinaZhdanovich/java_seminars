520ff4108fcf2243408c36252bd86a17373e0203
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-12-12 22:21:13 +0300

    Added Exceptions homework3

---
+++ src/exceptions_homework3/Information.java
@@ -0,0 +1,111 @@
+package exceptions_homework3;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+class InvalidDataException extends IllegalArgumentException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
+
+public class Information {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Введите Фамилию Имя Отчество, дату рождения (в формате dd.mm.yyyy), номер телефона(7xxxxxxxxxx), пол (f - женский, m - мужской), разделенные пробелом: ");
+            String userInput = scanner.nextLine();
+
+            String[] userData = userInput.split(" ");
+            if (userData.length != 6) {
+                throw new InvalidDataException("Неверное количество данных.");
+            }
+
+            String surname = userData[0].toUpperCase();
+            String name = userData[1].toUpperCase();
+            String patronymic = userData[2].toUpperCase();
+            String dateOfBirth = userData[3];
+            String phoneNumber = userData[4];
+            String gender = userData[5].toLowerCase();
+
+            List<String> errors = new ArrayList<>();
+
+            if (!validateName(surname)) {
+                errors.add("Некорректная фамилия.");
+            }
+
+            if (!validateName(name)) {
+                errors.add("Некорректное имя.");
+            }
+
+            if (!validateName(patronymic)) {
+                errors.add("Некорректное отчество.");
+            }
+
+            if (!validateDateOfBirth(dateOfBirth)) {
+                errors.add("Некорректная дата рождения. Введите в формате dd.mm.yyyy");
+            }
+
+            if (!validatePhoneNumber(phoneNumber)) {
+                errors.add("Некорректный формат номера телефона. Введите в формате 7xxxxxxxxxx.");
+            }
+
+            if (!validateGender(gender)) {
+                errors.add("Некорректный формат пола. Введите латиницей f - женский, m - мужской.");
+            }
+
+            if (!errors.isEmpty()) {
+                String errorMessage = String.join("\n", errors);
+                throw new InvalidDataException(errorMessage);
+            }
+            String fileName = surname + ".txt";
+            String userInfo = surname + " " + name + " " + patronymic + " " + dateOfBirth + " " + phoneNumber + " " + gender;
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+                writer.write(userInfo);
+                writer.newLine();
+                System.out.println("Информация успешно записана в файл " + fileName);
+            } catch (IOException e) {
+                System.out.println("Возникла ошибка при записи в файл: " + e.getMessage());
+            }
+        } catch (InvalidDataException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static boolean validateName(String name) {
+        return name.matches("[a-zA-Zа-яА-Я]+");
+    }
+
+    private static boolean validateDateOfBirth(String dateOfBirth) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate parsedDate = LocalDate.parse(dateOfBirth, formatter);
+            LocalDate currentDate = LocalDate.now();
+
+            return parsedDate.isBefore(currentDate) &&
+                    parsedDate.getYear() >= 1920 &&
+                    parsedDate.getMonthValue() <= 12 &&
+                    parsedDate.getDayOfMonth() <= 31;
+        } catch (DateTimeException e) {
+            return false;
+        }
+    }
+
+    private static boolean validatePhoneNumber(String phoneNumber) {
+        return phoneNumber.matches("\\d+") && phoneNumber.length() == 11;
+    }
+
+    private static boolean validateGender(String gender) {
+        return gender.matches("[fm]");
+    }
+}
